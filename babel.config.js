2f8c9f09d29b1126138a994c32b170f65688e05b
Artem Kovtun <rtmkovtn@ya.ru>
2020-04-27 15:07:20 +0800

    fixed package-lock.json

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
 const presets = [
-    [
-      "@babel/env",
-      {
-        useBuiltIns: "usage", // эта настройка babel-polyfill, если стоит значение usage, то будут подставлятся полифилы для версий браузеров которые указали ниже.
-        corejs: "3.4.1" // явно проставить версию corejs
-      }
-    ],
-  ];
-  
-  module.exports = { presets };
+  [
+    '@babel/env',
+    {
+      useBuiltIns: 'usage',
+      corejs: '3.4.1',
+    },
+  ],
+];
+
+module.exports = { presets };
--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
 module.exports = {
-    plugins: [
-        require('autoprefixer'),
-        require('cssnano')({
-            preset: 'default'
-    })
-    ]
-}
+  plugins: [
+    require('autoprefixer'),
+    require('cssnano')({
+      preset: 'default',
+    }),
+  ],
+};
