7f249af4fd1956eab502d3f76a8cf7ea233fbf37
Artem Kovtun <rtmkovtn@ya.ru>
2020-04-28 09:45:33 +0800

    fixed y-scroll on <640 res

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './index.css';
+import Popup from './script/Popup';
+
+const authButton = document.querySelector('.header__button');
+
+authButton.addEventListener('click', () => {
+  const popup = new Popup();
+  popup.render();
+});
